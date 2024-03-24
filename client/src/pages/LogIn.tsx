403e82c71ac989cb47660c87e05886d9d1f13b30
ved <61646336+VedavedyaBH@users.noreply.github.com>
2024-03-24 17:11:06 +0530

    Update LogIn.tsx

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -39,7 +39,7 @@ export function LogIn() {
     async function addUser(user: User) {
         try {
             if (user !== null) {
-                const url = `/api/v1/login`;
+                const url = `diary-ai-server.vercel.app/api/v1/login`;
                 const res = await axios.post(url, { user });
                 if (res.status === 200) {
                     const token = res.data;
