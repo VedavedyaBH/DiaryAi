f0c8cff08ef46e4afac526f20c3a979cccaf03cf
vedavedyabh <vedavedya007@gmail.com>
2024-04-07 14:38:12 +0530

    cleaned some

--- server/src/gemini/TitleGen.ts
+++ server/src/gemini/TitleGen.ts
@@ -1,19 +1,16 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from "dotenv";
 
-// Access your API key as an environment variable (see "Set up your API key" above)
-
 dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function generateTitle(content: string) {
-    // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
     const prompt = `Give a title for this ${content}.
     Do not respond with anthing else. Just the title.
-    Add emotions to it. Should not be very long.
-    Ignore html tags like <p><h1><pre> etc`;
+    Add emotions to it. Should not be very long. No emojis.
+    Just letters. Ignore html tags like <p><h1><pre> etc`;
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
@@ -22,7 +19,6 @@ export async function generateTitle(content: string) {
 }
 
 export async function generateTags(content: string) {
-    // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
     const prompt = `Give comma separated tags based on this ${content}.
@@ -30,7 +26,7 @@ export async function generateTags(content: string) {
     Minimum 1 tag. Tag's length should be small.
     Should be a word. Should not be very long.
     For example, if the content has "sad" emotions, give sad as tag.
-    Happy, success, lonley, betrayal are some of the examples,
+    Happy, success, lonley, betrayal are some of the examples.
     Ignore html tags like <p><h1><pre> etc`;
 
     const result = await model.generateContent(prompt);
@@ -40,11 +36,10 @@ export async function generateTags(content: string) {
 }
 
 export async function generateResponse(content: string) {
-    // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
     const prompt = `Give response to this ${content}. Console/motivate/cheer according to the content.
-    Keep it concise. Respond like a brother/father/mother/family/friend etc.
+    Respond like a family/friend etc. If needed, give suggestions/ideas/tips as well.
     Ignore html tags like <p><h1><pre> etc`;
 
     const result = await model.generateContent(prompt);
